cd05560434493931cc8c7d480c2aad520ec5d3fa
Rijo George <rijothedeveloper@gmail.com>
2021-12-17 10:53:50 -0800

    test framework done

---
+++ tests/test_app.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+from app import app
+
+class test_app(TestCase):
+    def test_home_page(self):
+        self.assertEqual(0,0)
---
+++ tests/test_models.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+from models import User
+
+class test_user(TestCase):
+    def test_user(self):
+        self.assertEqual(0,0)
